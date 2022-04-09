e5e639b22afcb3fb8086790bfbab3cc1b122b010
Michael Scherbakow <ryupold@gmail.com>
2022-04-09 13:29:46 +0200

    import manual_bindings.zig in raylib.zig

--- raylib.zig
+++ raylib.zig
@@ -4,3 +4,4 @@ pub usingnamespace @import("structs.zig");
 pub usingnamespace @import("enums.zig");
 pub usingnamespace @import("functions.zig");
 pub usingnamespace @import("constants.zig");
+pub usingnamespace @import("manual_bindings.zig");
