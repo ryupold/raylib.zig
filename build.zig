8ac502599eade5d4d02e672565e1f40189f4a6a3
Jose Rico <jmrico01@gmail.com>
2024-01-27 15:13:12 -0500

    initial setup

--- build.zig
+++ build.zig
@@ -1,26 +1,54 @@
 const std = @import("std");
+
 const generate = @import("generate.zig");
 
 pub fn build(b: *std.Build) !void {
-    const raylibSrc = "raylib/src/";
-
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
+    const raylib = b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Set up "raylib_zig" module for use as a dependency.
+    const module = b.addModule("raylib_zig", .{
+        .root_source_file = .{.path = "raylib.zig"},
+        .target = target,
+        .optimize = optimize,
+    });
+    module.addIncludePath(.{
+        .path = try b.build_root.join(b.allocator, &[_][]const u8 {"."})
+    });
+    module.addIncludePath(raylib.path("src"));
+    module.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            try b.build_root.join(b.allocator, &[_][]const u8 {"marshal.c"}),
+        },
+        .flags = &.{}
+    });
+    module.linkLibrary(raylib.artifact("raylib"));
+    module.link_libc = true;
+
+    // Set up binding generation library & exes.
     //--- parse raylib and generate JSONs for all signatures --------------------------------------
     const jsons = b.step("parse", "parse raylib headers and generate raylib jsons");
     const raylib_parser_build = b.addExecutable(.{
         .name = "raylib_parser",
-        .root_source_file = std.build.FileSource.relative("raylib_parser.zig"),
+        .root_source_file = .{.path = "raylib_parser.zig"},
         .target = target,
-        .optimize = .ReleaseFast,
+        .optimize = optimize,
     });
-    raylib_parser_build.addCSourceFile(.{ .file = .{ .path = "raylib/parser/raylib_parser.c" }, .flags = &.{} });
+    raylib_parser_build.addCSourceFile(.{
+        .file = raylib.path("parser/raylib_parser.c"),
+        .flags = &.{}
+    });
     raylib_parser_build.linkLibC();
 
     //raylib
     const raylib_H = b.addRunArtifact(raylib_parser_build);
     raylib_H.addArgs(&.{
-        "-i", raylibSrc ++ "raylib.h",
+        "-i", raylib.path("src/raylib.h").getPath(b),
         "-o", "raylib.json",
         "-f", "JSON",
         "-d", "RLAPI",
@@ -30,7 +58,7 @@ pub fn build(b: *std.Build) !void {
     //raymath
     const raymath_H = b.addRunArtifact(raylib_parser_build);
     raymath_H.addArgs(&.{
-        "-i", raylibSrc ++ "raymath.h",
+        "-i", raylib.path("src/raymath.h").getPath(b),
         "-o", "raymath.json",
         "-f", "JSON",
         "-d", "RMAPI",
@@ -40,7 +68,7 @@ pub fn build(b: *std.Build) !void {
     //rlgl
     const rlgl_H = b.addRunArtifact(raylib_parser_build);
     rlgl_H.addArgs(&.{
-        "-i", raylibSrc ++ "rlgl.h",
+        "-i", raylib.path("src/rlgl.h").getPath(b),
         "-o", "rlgl.json",
         "-f", "JSON",
         "-d", "RLAPI",
@@ -51,7 +79,7 @@ pub fn build(b: *std.Build) !void {
     const intermediate = b.step("intermediate", "generate intermediate representation of the results from 'zig build parse' (keep custom=true)");
     var intermediateZigStep = b.addRunArtifact(b.addExecutable(.{
         .name = "intermediate",
-        .root_source_file = std.build.FileSource.relative("intermediate.zig"),
+        .root_source_file = .{.path = "intermediate.zig"},
         .target = target,
     }));
     intermediate.dependOn(&intermediateZigStep.step);
@@ -60,7 +88,7 @@ pub fn build(b: *std.Build) !void {
     const bindings = b.step("bindings", "generate bindings in from bindings.json");
     var generateZigStep = b.addRunArtifact(b.addExecutable(.{
         .name = "generate",
-        .root_source_file = std.build.FileSource.relative("generate.zig"),
+        .root_source_file = .{.path = "generate.zig"},
         .target = target,
     }));
     const fmt = b.addFmt(.{ .paths = &.{generate.outputFile} });
@@ -71,79 +99,4 @@ pub fn build(b: *std.Build) !void {
     const raylib_parser_install = b.step("raylib_parser", "build ./zig-out/bin/raylib_parser.exe");
     const generateBindings_install = b.addInstallArtifact(raylib_parser_build, .{});
     raylib_parser_install.dependOn(&generateBindings_install.step);
-}
-
-// above: generate library
-// below: linking (use as dependency)
-
-fn current_file() []const u8 {
-    return @src().file;
-}
-
-const sep = std.fs.path.sep_str;
-const cwd = std.fs.path.dirname(current_file()).?;
-const dir_raylib = cwd ++ sep ++ "raylib" ++ sep ++ "src";
-
-const raylib_build = @import("raylib/src/build.zig");
-
-fn linkThisLibrary(b: *std.Build, target: std.Target.Query, optimize: std.builtin.Mode) *std.Build.Step.Compile {
-    const lib = b.addStaticLibrary(.{ .name = "raylib.zig", .target = b.resolveTargetQuery(target), .optimize = optimize });
-    lib.addIncludePath(.{ .path = dir_raylib });
-    lib.addIncludePath(.{ .path = cwd });
-    lib.linkLibC();
-    lib.addCSourceFile(.{ .file = .{ .path = cwd ++ sep ++ "marshal.c" }, .flags = &.{} });
-    std.log.info("include '{s}' to {s}", .{ dir_raylib, lib.name });
-    std.log.info("include '{s}' to {s}", .{ cwd, lib.name });
-    return lib;
-}
-
-/// add this package to exe
-pub fn addTo(b: *std.Build, exe: *std.Build.Step.Compile, target: std.Target.Query, optimize: std.builtin.Mode, raylibOptions: raylib_build.Options) void {
-    exe.root_module.addAnonymousImport("raylib", .{ .root_source_file = .{ .path = cwd ++ sep ++ "raylib.zig" } });
-    std.log.info("include '{s}' to {s}", .{ dir_raylib, exe.name });
-    std.log.info("include '{s}' to {s}", .{ cwd, exe.name });
-    exe.addIncludePath(.{ .path = dir_raylib });
-    exe.addIncludePath(.{ .path = cwd });
-    const lib = linkThisLibrary(b, target, optimize);
-    const lib_raylib = raylib_build.addRaylib(b, b.resolveTargetQuery(target), optimize, raylibOptions) catch |err| std.debug.panic("addRaylib: {any}", .{err});
-    exe.linkLibrary(lib_raylib);
-    exe.linkLibrary(lib);
-    std.log.info("linked raylib.zig", .{});
-}
-
-pub fn linkSystemDependencies(exe: *std.build.Step.Compile) void {
-    switch (exe.target.getOsTag()) {
-        .macos => {
-            exe.linkFramework("Foundation");
-            exe.linkFramework("Cocoa");
-            exe.linkFramework("OpenGL");
-            exe.linkFramework("CoreAudio");
-            exe.linkFramework("CoreVideo");
-            exe.linkFramework("IOKit");
-        },
-        .linux => {
-            exe.addLibraryPath(.{ .path = "/usr/lib" });
-            exe.addIncludePath(.{ .path = "/usr/include" });
-            exe.linkSystemLibrary("GL");
-            exe.linkSystemLibrary("rt");
-            exe.linkSystemLibrary("dl");
-            exe.linkSystemLibrary("m");
-            exe.linkSystemLibrary("X11");
-        },
-        .freebsd, .openbsd, .netbsd, .dragonfly => {
-            exe.linkSystemLibrary("GL");
-            exe.linkSystemLibrary("rt");
-            exe.linkSystemLibrary("dl");
-            exe.linkSystemLibrary("m");
-            exe.linkSystemLibrary("X11");
-            exe.linkSystemLibrary("Xrandr");
-            exe.linkSystemLibrary("Xinerama");
-            exe.linkSystemLibrary("Xi");
-            exe.linkSystemLibrary("Xxf86vm");
-            exe.linkSystemLibrary("Xcursor");
-        },
-        else => {},
-    }
-
-    exe.linkLibC();
 }
